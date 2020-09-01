4500111b0928073c957ddeb527de5884f2e06a76
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-01 08:33:40 -0600

    Currency

--- RunningTotal.vb
+++ RunningTotal.vb
@@ -1,16 +1,21 @@
 ﻿Option Strict On
 Option Explicit On
+Option Compare Text
 
 'Justine Hoffa
 'RCET0265
 'Fall 2020
+'Running Total
+'https://github.com/justinehoffa/RunningTotal
 
 Module RunningTotal
 
     Sub Main()
-        Dim userInput As String
-        Dim runningTotal As Integer
+        Dim userInput As String = ""
+        Dim runningTotal As Decimal
         Const TAXRATE As Decimal = 0.06D
+        Dim taxAmount As Decimal
+        Dim badData As Boolean = True
 
         While userInput <> "q"
 
@@ -18,26 +23,33 @@ Module RunningTotal
             Console.WriteLine("1. Add Item")
             Console.WriteLine("2. Calculate Tax")
 
-            'User Choice???
+            'User Choice
             userInput = Console.ReadLine()
 
 
             If userInput = "1" Then
-                Console.WriteLine("Enter the item cost.")
-                userInput = Console.ReadLine()
+                Do
+                    Console.WriteLine("Enter the item cost.")
+                    userInput = Console.ReadLine()
 
-                Try
-                    'runningTotal = runningTotal + Cint(userInput)
-                    runningTotal = CInt(userInput)
-                Catch ex As Exception
-                    'promit user to give good data
-                End Try
-                Console.WriteLine("Total: " & runningTotal)
+                    Try
+                        'runningTotal = runningTotal + CDec(userInput)
+                        runningTotal += CDec(userInput)
+                        badData = False
+                    Catch ex As Exception
+                        'promit user to give good data
+                        Console.WriteLine("Item cost must be a number.")
+                        badData = True
+                    End Try
+                Loop While badData
+
+                Console.WriteLine("Total: " & runningTotal.ToString("C"))
 
             ElseIf userInput = "2" Then
-                Console.WriteLine("Tax: " & runningTotal * TAXRATE)
+                taxAmount = runningTotal * TAXRATE
+                Console.WriteLine("Tax: " & taxAmount.ToString("C"))
             Else
-                'prompt user for good data
+                'catch all. bad choice
             End If
 
             Console.ReadLine()
